6d4af034337be1c334e30c7bf435787c2443e5ab
ecourse <smkang@codenuri.co.kr>
2022-07-12 14:11:27 +0900

    Add files via upload

--- DAY2/3_메뉴2.cpp
+++ DAY2/3_메뉴2.cpp
@@ -9,7 +9,7 @@ class MenuItem
 	std::string title;
 	int id;
 public:
-	MenuItem(const std::string& s) : title(s) {}
+	MenuItem(const std::string& s, int n) : title(s), id(n) {}
 
 	std::string get_title() const { return title; }
 
@@ -20,8 +20,12 @@ public:
 		_getch();
 	}
 };
+
 int main()
 {
+	// 핵심 : 메뉴 항목 하나가 "객체" 입니다.
+	//		 메뉴 항목이 20개 라면 20개의 객체를 생성하면 됩니다.
+	//	     메뉴가 선택(실행)되게 하려면 command()를 호출하면 됩니다.
 	MenuItem m1("김밥", 11);
 	MenuItem m2("라면", 12);
 	m1.command(); // 김밥 선택
