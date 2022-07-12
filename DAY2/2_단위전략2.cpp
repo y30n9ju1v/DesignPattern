a5daaad5e89f229d44a4c8223ce23a283a2d805b
ecourse <smkang@codenuri.co.kr>
2022-07-12 11:10:24 +0900

    Add files via upload

--- DAY2/2_단위전략2.cpp
+++ DAY2/2_단위전략2.cpp
@@ -2,6 +2,10 @@
 // 방법 1. 스레드 동기화 여부를 template method로 교체 가능하게..
 //	=> 변하는 것을 가상 함수로.
 
+// => 동기화 여부는 수많은 클래스에서 필요 하다.
+// => 그런데, template method 는 "동기화 정책"을 다른 클래스에서 사용할수 없다.
+//    (유연성 부족)
+
 template<typename T> class List
 {
 	// member data....
@@ -24,12 +28,13 @@ template<typename T> class ThreadSafeList : public List<T>
 {
 	std::mutex m;
 public:
-	void lock() { m.lock(); }
-	void unlock() { m.unlock(); }
+	void lock()   override { m.lock(); }
+	void unlock() override { m.unlock(); }
 };
 
 //List<int> st; 
 ThreadSafeList<int> st;
+
 
 int main()
 {
