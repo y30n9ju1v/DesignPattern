c75bae5a6044fbc71e34b7a6439bcda8e3355e4c
ecourse <smkang@codenuri.co.kr>
2022-07-12 09:33:04 +0900

    Add files via upload

--- DAY1/1_Edit2.cpp
+++ DAY1/1_Edit2.cpp
@@ -13,13 +13,11 @@ public:
 	// 핵심 1. 공통성과 가변성의 분리
 	// => 변하지 않은 코드 내부에 있는 변해야 하는 부분을 찾아서 분리한다.
 	// => 변하는 부분을 가상함수로 분리한다.
-
 	virtual bool validate(char c)
 	{
 		return true;
 	}
-
-
+	
 	std::string getData()
 	{
 		data.clear();
@@ -41,12 +39,22 @@ public:
 	}
 };
 
-
-
+// 이제 validation 정책을 변경하려면 "Edit"로 부터 파생 된 클래스를 제공합니다
+// => 기존 클래스(Edit) 의 변경이 아닌
+// => Edit 의 확장을 통한 기능의 변경
+class NumEdit : public Edit
+{
+public:
+	bool validate(char c) override
+	{
+		return isdigit(c);
+	}
+};
 
 int main()
 {
-	Edit e;
+//	Edit e;
+	NumEdit e;
 	while (1)
 	{
 		std::cout << e.getData() << std::endl;
