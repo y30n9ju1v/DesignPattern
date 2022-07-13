92dd3a38cc78c36abcc777f0e68ea55c37351ad7
ecourse <smkang@codenuri.co.kr>
2022-07-13 14:20:15 +0900

    Add files via upload

--- DAY3/QA2.cpp
+++ DAY3/QA2.cpp
@@ -21,13 +21,16 @@ class C2 : public X, public Shape
 int main()
 {
 	// 다중 상속이 좋지는 않지만 
-	// C++, python :지원
+	// C++, python : 지원
 	// C#, java    : 지원 안함(단 인터페이스의 경우 다중 상속가능)
 	C1 c1;
 	C2 c2;
-
 	Shape* p1 = &c1;
-	Shape* p2 = &c2;
+//	Shape* p2 = &c2; // (char*)&c2 + sizeof(1번째기반 클래스 X);
+
+	// 상속관계 고려 하지 말고, 무조건 &c2 주소를 Shape* 에 담겠다는 의도
+	// 강제적변환(reinterpret - 다시 해석하겠다)
+	Shape* p2 = reinterpret_cast<Shape*>(&c2);
 
 	std::cout << &c1 << std::endl;
 	std::cout << &c2 << std::endl;
