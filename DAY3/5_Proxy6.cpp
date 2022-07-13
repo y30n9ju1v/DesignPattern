480d1c34b363d43cead39ba47506792f25f3a86b
ecourse <smkang@codenuri.co.kr>
2022-07-13 16:24:45 +0900

    Add files via upload

--- DAY3/5_Proxy6.cpp
+++ DAY3/5_Proxy6.cpp
@@ -17,10 +17,26 @@ ICalc* ReloadProxy()
 
 int main()
 {
-	ICalc* pCalc = ReloadProxy();
+	ICalc* pCalc = ReloadProxy(); // 처음으로포인터를 얻으면
+
+	pCalc->AddRef();		// 참조 계수 증가
+
+	ICalc* pCalc2 = pCalc;	// 포인터를 복사해도
+
+	pCalc2->AddRef();		// 참조계수 증가
+
 
 	std::cout << pCalc->Add(10, 20) << std::endl;
 	std::cout << pCalc->Sub(10, 20) << std::endl;
+
+	// 사용후에는 delete 가 아니라 참조 계수 감소
+	pCalc->Release();  // 참조계수가 0이 아닌 1인 상태
+
+	std::cout << "------------" << std::endl;
+	pCalc2->Release(); // 이때 파괴..
+
+
+
 
 //	delete pCalc; // 좋은 코드일까요 ?
 				  // DLL 내부에서 new 를 했으므로 
