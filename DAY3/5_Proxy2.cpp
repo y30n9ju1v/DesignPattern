6daa338d3817c7c9d8c59aa74fd5aeede6bc12f7
ecourse <smkang@codenuri.co.kr>
2022-07-13 14:38:45 +0900

    Add files via upload

---
+++ DAY3/5_Proxy2.cpp
@@ -0,0 +1,28 @@
+#define USING_GUI
+#include "cppmaster.h"
+
+// 원격지 서버의 역활을 대신할 클래스를 설계 합니다.
+class CalcProxy
+{
+	int server_handle;
+public:
+	CalcProxy()
+	{
+		server_handle = ec_find_server("Calc");
+	}
+	int Add(int a, int b) { return ec_send_server(server_handle, 1, a, b); }
+	int Sub(int a, int b) { return ec_send_server(server_handle, 2, a, b); }
+};
+
+int main()
+{
+	CalcProxy* pCalc = new CalcProxy;
+
+	std::cout << pCalc->Add(10, 20) << std::endl;
+	std::cout << pCalc->Sub(10, 20) << std::endl;
+}
+
+
+
+
+
