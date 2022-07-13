ea66982228535b596d734c32067fa00d21a56617
ecourse <smkang@codenuri.co.kr>
2022-07-13 16:42:15 +0900

    Add files via upload

--- DAY3/5_Proxy7.cpp
+++ DAY3/5_Proxy7.cpp
@@ -17,18 +17,28 @@ template<typename T> class AutoPtr
 {
 	T* obj;
 public:
-	AutoPtr(T* p = nullptr)       : obj(p) {}
-	AutoPtr(const AutoPtr<T>& ap) : obj(ap.obj) {}
+	AutoPtr(T* p = nullptr)       : obj(p) { if (obj) obj->AddRef(); }
+	AutoPtr(const AutoPtr<T>& ap) : obj(ap.obj) { if (obj) obj->AddRef(); }
 
-	~AutoPtr() {}
+	~AutoPtr() { if (obj) obj->Release(); }
+
+	// 스마트 포인터의 핵심 : -> 와 * 연산자를 재정의해서 진짜 포인터처럼 보이게하다.
+	T* operator->() { return obj; }
+	T& operator*()  { return *obj; }
 };
-
 int main()
 {
-	AutoPtr<ICalc> p1 = ReloadProxy();
+	AutoPtr<ICalc> p1 = ReloadProxy(); // AutoPtr<ICalc> p1( ReloadProxy() );
 	AutoPtr<ICalc> p2 = p1;
-}
 
+	int n = p1->Add(10, 20); // 이 코드가 되어야 합니다.
+
+	std::cout << n << std::endl;
+}
+// 위코드가 결국 아래 코드와 동일합니다.
+// 그런데, 스마트포인터 덕분에 "AddRef/Release" 의 호출이 자동으로 됩니다.
+// 단, 위 AutoPtr 에는 대입연산자도 만들어야 합니다(숙제로 해보세요)
+// C++11 가능하신 분은 "move" 도 제공해 보세요
 
 
 /*
