f52aaaf3be4100ada0855071f78068450318b519
ecourse <smkang@codenuri.co.kr>
2022-07-13 13:07:11 +0900

    Add files via upload

--- DAY3/3_Decorator8.cpp
+++ DAY3/3_Decorator8.cpp
@@ -25,6 +25,37 @@ public:
 		printf("%s 쓰기\n", s.c_str());
 	}
 };
+//------------------------
+// 다양한 Stream 과 Stream 의 기능 추가 객체는 공통의 기반 클래스 필요
+// => Stream 으로 부터 파생..
+
+class ZipDecorator : public Stream
+{
+	Stream* stream;
+public:
+	ZipDecorator(Stream* s) : stream(s) {}
+
+	void Write(const std::string& s) override
+	{
+		std::string s2 = s + " 압축됨"; // 추가된 기능(압축) 실행후
+
+		stream->Write(s2);			   // stream 객체 전달
+	}
+};
+class EncryptDecorator : public Stream
+{
+	Stream* stream;
+public:
+	EncryptDecorator(Stream* s) : stream(s) {}
+
+	void Write(const std::string& s) override
+	{
+		std::string s2 = s + " 암호화"; 
+		stream->Write(s2);			   
+	}
+};
+
+
 
 int main()
 {
