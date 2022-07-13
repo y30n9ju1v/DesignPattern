e581d34d632c0a4fa08423fdd922ba125864e377
ecourse <smkang@codenuri.co.kr>
2022-07-13 10:11:00 +0900

    Add files via upload

--- DAY3/1_메뉴이벤트8.cpp
+++ DAY3/1_메뉴이벤트8.cpp
@@ -8,14 +8,15 @@ void foo(int a, int& b)
 {
     b = 100;
 }
-
 int main()
 {
     int n = 0;
 
     std::function<void(int)> f;
 
-    f = std::bind(&foo, _1, n);
+//  f = std::bind(&foo, _1, n); // n의 값을 고정된 값으로 보관합니다.
+    f = std::bind(&foo, _1, std::ref(n)); // n을 참조로 보관해 달라.
+
 
     f(5); // foo(5, n) 일까 ?  foo(5, 0) 일까 ?
 
