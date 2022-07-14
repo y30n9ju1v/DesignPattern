c33eb0291eb285904ff03809694a40e687287bfa
ecourse <smkang@codenuri.co.kr>
2022-07-14 13:08:02 +0900

    Add files via upload

---
+++ DAY4/3_Factory1.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include "Helper.h"
+
+class Shape
+{
+public:
+	virtual void draw() = 0;
+	virtual ~Shape() {}
+};
+
+class Rect : public Shape
+{
+public:
+	void draw() override { std::cout << "draw Rect" << std::endl; }
+};
+class Circle : public Shape
+{
+public:
+	void draw() override { std::cout << "draw Circle" << std::endl; }
+};
+
+// 도형을 만드는 팩토리
+
+class ShapeFactory
+{
+	MAKE_SINGLETON(ShapeFactory)
+public:
+	Shape* Create(int type)
+	{
+		Shape* p = nullptr;
+		if      (type == 1)	p = new Rect;
+		else if (type == 2)	p = new Circle;
+		return p;
+	}
+};
+
+
+int main()
+{
+	std::vector<Shape*> v;
+
+	ShapeFactory& factory = ShapeFactory::getInstance();
+
+	while (1)
+	{
+		int cmd;
+		std::cin >> cmd;
+
+		if (cmd >= 1 && cmd <= 7) // 1 ~ 7 번은 도형번호로 예약
+		{
+			Shape* p = factory.Create(cmd);
+
+			if (p != nullptr)
+				v.push_back(p);
+		}
+
+		else if (cmd == 9)
+		{
+			for (int i = 0; i < v.size(); i++)
+			{
+				v[i]->draw(); // 다형성
+							  // 동일한 표현식이 상황에 따라 다르게 동작
+							  // (실제 객체가 무엇인가에 따라 다른 함수 호출)
+							// => 새로운 도형이 추가되어도 이 한줄은 변하지 않는다
+							// => OCP
+
+			}
+		}
+	}
+}
+
+
+
+
