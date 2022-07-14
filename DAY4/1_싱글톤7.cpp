11edb1c32fa977cbabd31879da6588fa643a125a
ecourse <smkang@codenuri.co.kr>
2022-07-14 10:32:27 +0900

    Add files via upload

--- DAY4/1_싱글톤7.cpp
+++ DAY4/1_싱글톤7.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
 
 class Cursor
 {
 private:
-	Cursor() {}
+	Cursor() 
+	{
+		std::cout << "start ctor" << std::endl;
+
+		std::this_thread::sleep_for(std::chrono::seconds(10));
+
+		std::cout << "end ctor" << std::endl;
+	}
 
 	Cursor(const Cursor&) = delete;
 	Cursor& operator=(const Cursor&) = delete;
@@ -11,18 +20,26 @@ private:
 public:
 	static Cursor& getInstance()
 	{
+		std::cout << "start getInstance : " << std::this_thread::get_id()
+			      << std::endl;
+
 		static Cursor instance;
+
+		std::cout << "end getInstance : " << std::this_thread::get_id()
+			<< std::endl;
 		return instance;
 	}
 };
-
 int main()
 {
-	Cursor& c1 = Cursor::getInstance();
-	Cursor& c2 = Cursor::getInstance();
+	std::thread t1(&Cursor::getInstance);
 
-	std::cout << &c1 << std::endl;
-	std::cout << &c2 << std::endl;
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+
+	std::thread t2(&Cursor::getInstance);
+
+	t1.join();
+	t2.join();
 
 }
 
