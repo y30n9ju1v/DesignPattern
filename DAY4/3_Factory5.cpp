6756170835f713f78bb3c848ea00a9d78d85485e
ecourse <smkang@codenuri.co.kr>
2022-07-14 14:25:26 +0900

    Add files via upload

--- DAY4/3_Factory5.cpp
+++ DAY4/3_Factory5.cpp
@@ -8,6 +8,13 @@ class Shape
 public:
 	virtual void draw() = 0;
 	virtual ~Shape() {}
+
+
+	// prototype 패턴 : 자신의 복사본을 만드는 가상함수를 재공하는 디자인 패턴
+	// 공장에 견본 객체를 등록해 놓고..
+	// 필요할때 마다 "Clone()" 을 호출해서 복사본을 만들어서 사용
+
+	virtual Shape* Clone() = 0;
 };
 
 class Rect : public Shape
@@ -15,31 +22,10 @@ class Rect : public Shape
 public:
 	void draw() override { std::cout << "draw Rect" << std::endl; }
 
-	// 클래스가 자신의 객체를 만드는 "static 멤버 함수"를 제공하는 기술은
-	// 아주 유용하고 널리 사용됩니다.
 	static Shape* Create() { return new Rect; }
-};
-// Rect객체를 만드는 방법
-// 1. Rect* p1 = new Rect;
-// 2. Rect* p2 = Rect::Create();
 
-/*
-void fn(int arg)
-{
-	// 함수인자(arg)따라 다른 도형을 만들고 싶다.
-	// 단, 미래에 만들어질 도형까지 !!
-	if (arg == 1) new Rect; //이렇게 하면 클래스 이름을 알아야 합니다
-							// 미래의 도형은 클래스 이름을 알수 없습니다.
-}
-*/
-/*
-void fn(Shape* (*f)())
-{
-	Shape* p = f(); // 객체 생성
-}
-fn( &Rect::Create ); // 이 코드는 결국 "클래스이름"을 인자로 보내는 의미 입니다.
-fn( &Triangle::Create);
-*/
+	Shape* Clone() override { return new Rect(*this); }
+};
 
 class Circle : public Shape
 {
@@ -47,32 +33,30 @@ public:
 	void draw() override { std::cout << "draw Circle" << std::endl; }
 
 	static Shape* Create() { return new Circle; }
+
+	Shape* Clone() override { return new Circle(*this); }
 };
 
 class ShapeFactory
 {
 	MAKE_SINGLETON(ShapeFactory)
-
-
-		using CREATOR = Shape * (*)();
-
-	std::map<int, CREATOR> create_map; // {도형번호, 생성함수}
-
+	std::map<int, Shape*> prototype_map; // 견본 도형 맵
 public:
-	void Register(int type, CREATOR f) { create_map[type] = f; }
-
+	void Register(int type, Shape* sample) 
+	{
+		prototype_map[type] = sample;
+	}
 
 	Shape* Create(int type)
 	{
 		Shape* p = nullptr;
 
-		auto ret = create_map.find(type);
+		auto ret = prototype_map.find(type);
 
-		if (ret != create_map.end())
+		if (ret != prototype_map.end())
 		{
-			// ret 는 요소의 포인터 이므로 pair 의 포인터입니다.
-			// pair.first 가 key, pair.second 가 value(함수포인터)
-			p = (ret->second)();
+			p = prototype_map[type]->Clone(); // 기존 도형의 복사본으로
+											// 새로운 도형 생성
 		}
 		return p;
 	}
