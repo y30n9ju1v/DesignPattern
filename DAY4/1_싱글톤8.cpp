ade0677ae26f7e4d3f7253e58fabe30b782f2d6b
ecourse <smkang@codenuri.co.kr>
2022-07-14 10:40:04 +0900

    Add files via upload

--- DAY4/1_싱글톤8.cpp
+++ DAY4/1_싱글톤8.cpp
@@ -3,25 +3,31 @@
 class Cursor
 {
 private:
-	Cursor() {}
+	Cursor() 
+	{
+		std::cout << "Cursor()" << std::endl;
+	}
 
 	Cursor(const Cursor&) = delete;
 	Cursor& operator=(const Cursor&) = delete;
 
+//	Cursor instance; // error. 이 코드는 에러 지만
+	static Cursor instance;  // 이 코드는 에러 아닙니다.
+
 public:
 	static Cursor& getInstance()
 	{
-		static Cursor instance;
+//		static Cursor instance; // 이 코드는 
+								// 이 함수가 처음 실행될때 생성자가 호출됩니다.
 		return instance;
 	}
 };
+Cursor Cursor::instance;
+
 int main()
 {
-	Cursor& c1 = Cursor::getInstance();
-	Cursor& c2 = Cursor::getInstance();
-
-	std::cout << &c1 << std::endl;
-	std::cout << &c2 << std::endl;
+//	Cursor& c1 = Cursor::getInstance();
+//	Cursor& c2 = Cursor::getInstance();
 
 }
 
