b42bea37e67d68c799c1c28a8e2e30711f4f50f4
ecourse <smkang@codenuri.co.kr>
2022-07-14 09:32:00 +0900

    Add files via upload

---
+++ DAY4/1_싱글톤3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+class Cursor
+{
+private:
+	Cursor() {}
+	Cursor(const Cursor&) = delete;
+	Cursor& operator=(const Cursor&) = delete;
+public:
+	static Cursor& getInstance()
+	{
+		static Cursor instance;
+		return instance;
+	}
+};
+int main()
+{
+	Cursor& c1 = Cursor::getInstance();
+	Cursor& c2 = Cursor::getInstance();
+
+	std::cout << &c1 << std::endl;
+	std::cout << &c2 << std::endl;
+}
+
+
+
+
+
+
+
+
