9d7c89f8bfa8047776c255046ac789b97d031f1d
ecourse <smkang@codenuri.co.kr>
2022-07-14 14:39:44 +0900

    Add files via upload

---
+++ DAY4/4_Command1.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include "Helper.h"
+
+class Shape
+{
+public:
+	virtual void draw() = 0;
+	virtual ~Shape() {}
+};
+
+class Rect : public Shape
+{
+public:
+	void draw() override { std::cout << "draw Rect" << std::endl; }
+};
+class Circle : public Shape
+{
+public:
+	void draw() override { std::cout << "draw Circle" << std::endl; }
+};
+
+//----------------------
+// Command 패턴 : 명령을 캡슐화 해서, 명령의 저장/취소/복구가 가능하게 한다.
+
+struct ICommand
+{
+	virtual void Execute() = 0;
+	virtual bool CanUndo() { return false; }
+	virtual void Undo() {}
+	virtual ~ICommand() {}
+};
+
+
+
+
+
+
+int main()
+{
+	std::vector<Shape*> v;
+
+	while (1)
+	{
+		int cmd;
+		std::cin >> cmd;
+
+		if (cmd == 1)
+		{
+			v.push_back(new Rect);
+		}
+		else if (cmd == 2)
+		{
+			v.push_back(new Circle);
+		}
+		else if (cmd == 9)
+		{
+			for (int i = 0; i < v.size(); i++)
+			{
+				v[i]->draw();
+			}
+		}
+	}
+}
+
+
+
+
