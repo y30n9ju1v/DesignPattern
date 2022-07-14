7bd1d6e6f4cfb04a1dc4ef4eab33d6cfed3b8899
ecourse <smkang@codenuri.co.kr>
2022-07-14 10:17:53 +0900

    Add files via upload

--- DAY4/1_싱글톤6.cpp
+++ DAY4/1_싱글톤6.cpp
@@ -1,46 +1,50 @@
 #include <iostream>
 #include <mutex>
 
-class Cursor
+// CRTP : Curiously Recurring Template Pattern (C++ IDioms)
+
+// => 기반 클래스 설계시 파생 클래스의 이름을 사용할수 있게 하는 기술
+// => 기반 클래스가 템플릿 인데, 파생 클래스 만들때 자신의 이름을 템플릿 인자로
+//    전달하는 기술
+
+template<typename TYPE>
+class Singleton
 {
+protected:
+	Singleton() {}
+
 private:
-	Cursor() {}
-	Cursor(const Cursor&) = delete;
-	Cursor& operator=(const Cursor&) = delete;
+	Singleton(const Singleton&) = delete;
+	Singleton& operator=(const Singleton&) = delete;
 
 	static std::mutex mtx;
-	static Cursor* sInstance;
+	static TYPE* sInstance;  // <<=====
 public:
-
-
-	static Cursor& getInstance()
+	static TYPE& getInstance() // <====
 	{
 		std::lock_guard<std::mutex> g(mtx); 
 
 		if (sInstance == nullptr)
 		{
-			sInstance = new Cursor;
+			sInstance = new TYPE;  // <===
 		}
-
-
 		return *sInstance;
 	}
+};
+template<typename TYPE> std::mutex Singleton<TYPE>::mtx;
+template<typename TYPE> TYPE* Singleton<TYPE>::sInstance = nullptr;
 
+
+// Keyboard 도 힙에 만드는 싱글톤 기법을 사용하고 싶다.
+class Keyboard : public Singleton< Keyboard >
+{
 
 };
-std::mutex Cursor::mtx;
-Cursor* Cursor::sInstance = nullptr;
-
 
 
 
 int main()
 {
-	Cursor& c1 = Cursor::getInstance();
-	Cursor& c2 = Cursor::getInstance();
-
-	std::cout << &c1 << std::endl;
-	std::cout << &c2 << std::endl;
 }
 
 
