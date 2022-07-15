5c75323831c36d539b8a0c4acf226abf6d86dbab
ecourse <smkang@codenuri.co.kr>
2022-07-15 13:23:28 +0900

    Add files via upload

--- DAY5/2_방문자패턴2.cpp
+++ DAY5/2_방문자패턴2.cpp
@@ -13,7 +13,7 @@ template<typename T> struct IVisitor
 
 // 이제 다양한 방문자를 설계 합니다.
 // 방문자는 "요소 한개에 대한 연산" 을 정의 하면 됩니다.
-template<typename T> class TWiceVisitor : public IVisitor<T>
+template<typename T> class TwiceVisitor : public IVisitor<T>
 {
 public:
 	void visit(T& v) override { v *= 2; }
@@ -32,31 +32,32 @@ template<typename T>
 class MyList : public std::list<T>, public IAccept<T>
 {
 public:
+	// std::list 의 모든 생성자를 그대로 사용할수 있게 해달라!!
+	// => C++11 에서 추가된 생성자 상속
+	
+	using std::list<T>::list;
+
+
 	void Accept(IVisitor<T>* visitor) override
 	{
 		// 방문자 : 한개의 요소에 대한 연산 수행
 		// 따라서, 자신의 모든 요소를 방문자에 전달합니다.
-		for (auto& e : *this)
+
+		for (auto& e : *this)   // "*this" : 결국 std::list 입니다.
 			visitor->visit(e);
 
 	}
 };
 
-
-
-
-
-
-
 int main()
 {
-	std::list<int> s = { 1,2,3,4,5,6,7,8,9,10 };
+	MyList<int> s = { 1,2,3,4,5,6,7,8,9,10 };
 
 	TwiceVisitor<int> tv; 
 	s.Accept(&tv);
 
-	ShowVisitor<int> sv; 
-	s.Accept(&sv);
+//	ShowVisitor<int> sv; 
+//	s.Accept(&sv);
 
 }
 
